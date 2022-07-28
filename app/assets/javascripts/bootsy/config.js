2da6ee3c47d34d9edb8ec3d97b6b578c4a668560
Arman Yerkesh <a.yerkesh@gmail.com>
2022-07-28 11:49:53 +0300

    Change Bootsy.config.remoteGalleryUrl

--- app/assets/javascripts/bootsy/config.js
+++ app/assets/javascripts/bootsy/config.js
@@ -2,5 +2,5 @@
 window.Bootsy = window.Bootsy || {};
 
 Bootsy.config = {
-  remoteGalleryURL: 'http://localhost:3000/user_files.json?filetype=image&page=1&per_page=10&school_id=1'
+  remoteGalleryURL: 'http://localhost:3000'
 }
