13c7555862f4e8d097f04f827758cea6261f5dcd
Arman Yerkesh <a.yerkesh@gmail.com>
2023-12-14 08:15:23 +0200

    Display "There are currently no uploaded images." text instead of empty alert

--- app/assets/javascripts/bootsy/modal.js
+++ app/assets/javascripts/bootsy/modal.js
@@ -268,6 +268,7 @@ Bootsy.Modal.prototype.hideUploadLoadingAnimation = function() {
 
 // Alert for empty gallery
 Bootsy.Modal.prototype.showEmptyAlert = function() {
+  this.$el.find('.bootsy-empty-alert').text(window.Bootsy.translations['noImagesUploaded']);
   this.$el.find('.bootsy-empty-alert').fadeIn(200);
 };
 
