50f0a5244e2c8a3bcd31018f26be9431f264bc0b
Arman Yerkesh <a.yerkesh@gmail.com>
2023-12-22 11:19:21 +0200

    Move current_translations from Bootsy's application helper to dummy app's application helper

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def current_translations
+    @translations ||= I18n.backend.send(:translations)
+    @translations[I18n.locale].with_indifferent_access
+  end
 end
