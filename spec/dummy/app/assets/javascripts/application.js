b3adae8cbe40cea8db603b07c18a95b481f587b5
Arman Yerkesh <a.yerkesh@gmail.com>
2025-12-30 08:47:19 +0200

    Add possibility to add YouTube shorts

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -30,6 +30,6 @@ Bootsy.config ||= {};
 Bootsy.config.backendURL = 'http://localhost:3000';
 Bootsy.config.page = 1;
 Bootsy.config.regexes = Object.freeze({
-  YouTube: /(?:youtube\.com\/(?:watch\?v=|embed\/|v\/)|youtu\.be\/)([\w-]{11})/,
+  YouTube: /(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/)|youtu\.be\/)([\w-]{11})/,
   Vimeo: /vimeo\.com\/(?:video\/)?(\d+)/
 });
